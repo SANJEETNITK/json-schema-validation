b47d39465b4dbdf31dadf2d2993bcd3f8b9faeb8
Sanjeet <sanjeet677088@gmail.com>
2023-02-16 00:22:55 +0530

    added swagger implementation in a spring boot project

---
+++ swagger-demo/README.md
@@ -0,0 +1,1 @@
+# This project is an example for Swagger implementation in Spring boot project.
---
+++ swagger-demo/src/main/java/swaggerdemo/config/SwaggerConfig.java
@@ -0,0 +1,107 @@
+//package swaggerdemo.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.builders.RequestHandlerSelectors;
+//import springfox.documentation.service.ApiInfo;
+//import springfox.documentation.service.Contact;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spring.web.plugins.Docket;
+//
+//import java.util.Collections;
+//
+//@Configuration
+//@EnableWebMvc
+//public class SwaggerConfig {
+//
+//    @Bean
+//    public Docket docket() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select()
+//                .apis(RequestHandlerSelectors.any())
+//                .paths(PathSelectors.any())
+//                .build()
+//                .apiInfo(metaInfo());
+//    }
+//
+//    private ApiInfo metaInfo() {
+//        return new ApiInfo("Rest APIs",
+//                "List of APIs built",
+//                "1.0",
+//                "Terms of service",
+//                new Contact("test", "www.org.com", "test@emaildomain.com"),
+//                "License of API",
+//                "API license URL",
+//                Collections.emptyList());
+//    }
+//}
+
+
+
+package swaggerdemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.*;
+
+import java.util.Collections;
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig {
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "My REST API",
+                "Some custom description of API.",
+                "v1.0.0",
+                "Terms of service",
+                new Contact("Sanjeet Kumar", "www.example.com", "myeaddress@company.com"),
+                "License of API",
+                "API license URL",
+                Collections.emptyList());
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    /**
+     * SwaggerUI information
+     */
+    @Bean
+    UiConfiguration uiConfig() {
+        return UiConfigurationBuilder.builder()
+                .deepLinking(true)
+                .displayOperationId(false)
+                .defaultModelsExpandDepth(-1) // 1 to show and -1 to hide -> https://swagger.io/docs/open-source-tools/swagger-ui/usage/configuration/
+                .defaultModelExpandDepth(1)
+                .defaultModelRendering(ModelRendering.EXAMPLE)
+                .displayRequestDuration(false)
+                .docExpansion(DocExpansion.NONE)
+                .filter(false)
+                .maxDisplayedTags(null)
+                .operationsSorter(OperationsSorter.ALPHA)
+                .showExtensions(false)
+                .tagsSorter(TagsSorter.ALPHA)
+                .supportedSubmitMethods(UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS)
+                .validatorUrl(null)
+                .build();
+    }
+}
